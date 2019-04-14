c784ba01bbee31303ebaa9eb404b9e96430b985f
WangJ-PC-P2 <jie581825@yeah.net>
2019-04-14 16:07:12 +0800

    LogUtil-v1.0 finish.

--- app/src/main/java/com/wangj/arsenal/ProjectApplication.java
+++ app/src/main/java/com/wangj/arsenal/ProjectApplication.java
@@ -2,11 +2,17 @@ package com.wangj.arsenal;
 
 import android.app.Application;
 import android.content.res.Configuration;
+import android.util.Log;
+
+import com.wangj.fun.util.LogUtil;
 
 public class ProjectApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // 设置LogUtil工具打印日志级别
+        LogUtil.setLogLevel(Log.DEBUG);
     }
 
     @Override
