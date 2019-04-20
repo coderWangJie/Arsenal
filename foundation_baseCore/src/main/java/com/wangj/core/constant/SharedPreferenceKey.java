fa38e1f1a634998ddeb5e6b7c46692b1eda399e0
WangJ-PC-P2 <jie581825@yeah.net>
2019-04-20 15:10:04 +0800

    SharedPreferencesUtil

---
+++ foundation_baseCore/src/main/java/com/wangj/core/constant/SharedPreferenceKey.java
@@ -0,0 +1,27 @@
+package com.wangj.core.constant;
+
+/**
+ *  此常量类被{@link com.wangj.core.util.SharedPreferenceUtil}使用。 <br>
+ *
+ *  SharedPreference定义键名规则：Function_Name_S<br>
+ *      Function:功能前缀，如User表示和用户相关<br>
+ *      Name：关键键名称，和存储值的含义密切关联，建议起名能够准确表达含义<br>
+ *      S：该键存储值的数据类型（SharedPreference支持的数据类型，String用S，
+ *                                                          StringSet用SSet
+ *                                                          int用I，
+ *                                                          boolean用B，
+ *                                                          long用L，
+ *                                                          float用F）
+ *
+ * @author Wang email:jie581825@yeah.net 2019/4/15 
+ * @version 1.0
+ *          Modified on 2019/4/15 by TODO
+ *          Modified: TODO
+ *
+ * if you modify some, PLEASE RECORD YOUR MODIFY !!!
+ */
+public class SharedPreferenceKey {
+
+    /**  */
+    public static final String User_Name_S = "";
+}
---
+++ foundation_baseCore/src/main/java/com/wangj/core/util/SharedPreferenceUtil.java
@@ -0,0 +1,89 @@
+package com.wangj.core.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.wangj.core.android.BaseApplication;
+import com.wangj.core.constant.SharedPreferenceKey;
+
+import java.util.Set;
+
+/**
+ * SharedPreference工具类<br>
+ * 建议将键名定义在{@link SharedPreferenceKey}中，避免在代码中直接随处起名导致混乱难以管理
+ *
+ * @author Wang email:jie581825@yeah.net
+ * @version 1.0
+ * <p>
+ * if you modify some, PLEASE RECORD YOUR MODIFY !!!
+ * @since 2019/4/8 20:59
+ */
+public class SharedPreferenceUtil {
+
+    private static SharedPreferences sharedPreferences;
+
+    static {
+        sharedPreferences = BaseApplication.getAppContext()
+                .getSharedPreferences("AppSharedPrf", Context.MODE_PRIVATE);
+    }
+
+    public static void setInfoToShared(String name, String newValue) {
+        sharedPreferences.edit().putString(name, newValue).apply();
+    }
+
+    public static String getString(String name) {
+        return sharedPreferences.getString(name, "");
+    }
+
+    public static String getString(String name, String defValue) {
+        return sharedPreferences.getString(name, defValue);
+    }
+
+    public static void setInfoToShared(String name, Set<String> newValue) {
+        sharedPreferences.edit()
+                .putStringSet(name, newValue)
+                .apply();
+    }
+
+    public static Set<String> getStringSet(String name, Set<String> defValue) {
+        return sharedPreferences.getStringSet(name, defValue);
+    }
+
+    public static void setInfoToShared(String name, boolean newValue) {
+        sharedPreferences.edit().putBoolean(name, newValue).apply();
+    }
+
+    public static boolean getBoolean(String name, boolean defValue) {
+        return sharedPreferences.getBoolean(name, defValue);
+    }
+
+    public static void setInfoToShared(String name, int newValue) {
+        sharedPreferences.edit()
+                .putInt(name, newValue)
+                .apply();
+    }
+
+    public static int getInt(String name, int defValue) {
+        return sharedPreferences.getInt(name, defValue);
+    }
+
+    public static void setInfoToShared(String name, float newValue) {
+        sharedPreferences.edit()
+                .putFloat(name, newValue)
+                .apply();
+    }
+
+    public static float getFloat(String name, float defValue) {
+        return sharedPreferences.getFloat(name, defValue);
+    }
+
+    public static void setInfoToShared(String name, long newValue) {
+        sharedPreferences.edit()
+                .putLong(name, newValue)
+                .apply();
+    }
+
+    public static long getLong(String name, long defValue) {
+        return sharedPreferences.getLong(name, defValue);
+    }
+}
