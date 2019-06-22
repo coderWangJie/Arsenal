5ebf3d6637145e5d7ad76dc2cca42e2c94c1a603
WangJ <jie581825@yeah.net>
2019-06-22 17:38:33 +0800

    代码暂存

--- test_demo/src/main/java/com/wangj/demo/TabLayoutRecyclerView2Activity.java
+++ test_demo/src/main/java/com/wangj/demo/TabLayoutRecyclerView2Activity.java
@@ -1,17 +1,20 @@
 package com.wangj.demo;
 
+import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.wangj.core.ARoutMapping;
 import com.wangj.core.android.BaseActivity;
-import com.wangj.demo.model.TabViewPagerVo;
+import com.wangj.core.util.LogUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,15 +27,15 @@ public class TabLayoutRecyclerView2Activity extends BaseActivity {
     @BindView(R2.id.tabLayout)
     TabLayout tabLayout;
 
-    @BindView(R2.id.viewPager)
-    ViewPager viewPager;
+    @BindView(R2.id.recyclerView)
+    RecyclerView recyclerView;
 
-    FragmentManager manager;
-    private FragmentTransaction transtion;
+    private List<String> dataList;
 
-    private List<TabViewPagerVo> dataList;
+    LinearLayoutManager linearLayoutManager;
 
-    private MyFragmentPageAdapter adapter;
+    int lastPos;
+    int recyclerViewScrollState = 0;
 
     @Override
     protected int getContentLayoutRes() {
@@ -47,74 +50,130 @@ public class TabLayoutRecyclerView2Activity extends BaseActivity {
     @Override
     protected void doBeforeResume() {
         dataList = new ArrayList<>();
+        dataList.add("这个是第一个");
+        dataList.add("2");
+        dataList.add("3");
+        dataList.add("I'am Fourth");
+        dataList.add("第5个");
+        dataList.add("第六");
+        dataList.add("第7");
+        dataList.add("第88888888888888");
+        dataList.add("第99");
 
-        TabViewPagerVo item1 = new TabViewPagerVo("这个是第一个", new TabFragment1());
-        dataList.add(item1);
+        MyAdapter adapter = new MyAdapter();
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
 
-        TabViewPagerVo item2 = new TabViewPagerVo("2", new TabFragment2());
-        dataList.add(item2);
+        for (int i = 0; i < dataList.size(); i++) {
+            tabLayout.addTab(tabLayout.newTab(), i == 0);
 
-        TabViewPagerVo item3 = new TabViewPagerVo("3", new TabFragment3());
-        dataList.add(item3);
+            View view = getLayoutInflater().inflate(R.layout.demo_item_tab_title, null);
+            ((TextView) view.findViewById(R.id.tvTitle)).setText(dataList.get(i));
+            tabLayout.getTabAt(i).setCustomView(view);
+        }
 
-        TabViewPagerVo item4 = new TabViewPagerVo("I'am Fourth", new TabFragment4());
-        dataList.add(item4);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
 
-        TabViewPagerVo item5 = new TabViewPagerVo("第5个", new TabFragment1());
-        dataList.add(item5);
+                LogUtil.d("WangJ", "newState:" + newState);
+                recyclerViewScrollState = newState;
+            }
 
-        TabViewPagerVo item6 = new TabViewPagerVo("第六", new TabFragment2());
-        dataList.add(item6);
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
 
-        //使用manager和transtion提交事务后为viewpager设置一个适配器
-        manager = getSupportFragmentManager();
-        adapter = new MyFragmentPageAdapter(manager, dataList);
-        transtion = manager.beginTransaction();
-        transtion.commit();
-        viewPager.setAdapter(adapter);
+                if (lastPos != linearLayoutManager.findFirstVisibleItemPosition()) {
+                    lastPos = linearLayoutManager.findFirstVisibleItemPosition();
+//                    tabLayout.setScrollPosition(lastPos, 0, true);
+                    tabLayout.getTabAt(lastPos).select();
+                }
+            }
+        });
 
-        // tablayout.addTab可以将标题添加进Tab里面，true表示默认选中
-        tabLayout.addTab(tabLayout.newTab().setText(dataList.get(0).getTitle()), true);
-        tabLayout.addTab(tabLayout.newTab().setText(dataList.get(1).getTitle()), false);
-        tabLayout.addTab(tabLayout.newTab().setText(dataList.get(2).getTitle()), false);
-        tabLayout.addTab(tabLayout.newTab().setText(dataList.get(3).getTitle()), false);
-        tabLayout.addTab(tabLayout.newTab().setText(dataList.get(4).getTitle()), false);
-        tabLayout.addTab(tabLayout.newTab().setText(dataList.get(5).getTitle()), false);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                View view = tab.getCustomView();
 
-        //这两个方法是将Tablayout和Viewpager联合起来
-        tabLayout.setupWithViewPager(viewPager);
-        //tablayout.setTabsFromPagerAdapter(adapter);
+                TextView tvTitle = view.findViewById(R.id.tvTitle);
+                tvTitle.getPaint().setFakeBoldText(true);
+                // 设置粗体后强制刷新一下TextView，否则有可能在切换中设置失效，导致粗体/正常体切换失效---验证发现整个刷view无效---
+                tvTitle.postInvalidate();
+
+                ImageView imgIcon = view.findViewById(R.id.imgIcon);
+                imgIcon.setImageResource(R.drawable.ui_face_positive);
+
+                if (recyclerViewScrollState == 0) {
+                    linearLayoutManager.scrollToPositionWithOffset(tabLayout.getSelectedTabPosition(), 0);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                View view = tab.getCustomView();
+
+                TextView tvTitle = view.findViewById(R.id.tvTitle);
+                tvTitle.getPaint().setFakeBoldText(false);
+                // 设置粗体后强制刷新一下TextView，否则有可能在切换中设置失效，导致粗体/正常体切换失效---验证发现整个刷view无效---
+                tvTitle.postInvalidate();
+
+                ImageView imgIcon = view.findViewById(R.id.imgIcon);
+                imgIcon.setImageResource(R.drawable.ui_frame_face_neutral);
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                View view = tab.getCustomView();
+
+                TextView tvTitle = view.findViewById(R.id.tvTitle);
+                tvTitle.getPaint().setFakeBoldText(true);
+                // 设置粗体后强制刷新一下TextView，否则有可能在切换中设置失效，导致粗体/正常体切换失效---验证发现整个刷view无效---
+                tvTitle.postInvalidate();
+
+                ImageView imgIcon = view.findViewById(R.id.imgIcon);
+                imgIcon.setImageResource(R.drawable.ui_face_positive);
+
+                if (recyclerViewScrollState == 0) {
+                    linearLayoutManager.scrollToPositionWithOffset(tabLayout.getSelectedTabPosition(), 0);
+                }
+            }
+        });
 
     }
 
 
-    private class MyFragmentPageAdapter extends FragmentPagerAdapter {
+    class MyAdapter extends RecyclerView.Adapter<MyHolder> {
 
-        private List<TabViewPagerVo> list;
-
-        public MyFragmentPageAdapter(FragmentManager fm, List<TabViewPagerVo> list) {
-            super(fm);
-            this.list = list;
+        @NonNull
+        @Override
+        public MyHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            return new MyHolder(LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.demo_item_test, viewGroup, false));
         }
 
         @Override
-        public Fragment getItem(int position) {
-            return list.get(position).getFragment();
+        public void onBindViewHolder(@NonNull MyHolder myHolder, int i) {
+            myHolder.textView.setText(dataList.get(i));
+            myHolder.textView.setBackgroundColor(Color.rgb((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255)));
         }
 
         @Override
-        public int getCount() {
-            return list.size();
+        public int getItemCount() {
+            return dataList.size();
         }
+    }
 
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return list.get(position).getTitle();
-        }
+    class MyHolder extends RecyclerView.ViewHolder {
 
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            super.destroyItem(container, position, object);
+        TextView textView;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.tv);
+            textView.setHeight(800);
         }
     }
 }
